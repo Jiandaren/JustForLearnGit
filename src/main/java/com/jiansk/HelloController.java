46c9e0684e15ab52fb2477ed506e23c78505498f
jiansk20361 <jiansk20361@hundsun.com>
2017-06-10 18:58:48 +0800

    wrote a new txt file

---
+++ src/main/java/com/jiansk/HelloController.java
@@ -0,0 +1,12 @@
+package com.jiansk;
+
+/*********************************************************
+ * 文件名称：HelloController
+ * 模块名称：com.jiansk 
+ * 功能说明：spingBootDemo 
+ * 开发人员：Administrator
+ * 开发时间：2017/4/23 23:36
+ * 修改记录：
+ *********************************************************/
+public class HelloController {
+}
